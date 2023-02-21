d917f5a1da8e0b06bee87d92e7421f0b6cc16ce7
tanmayabiswal <tan.biswal@rapidfunnel.com>
2023-02-21 18:40:07 +0530

    Add Expense Filter Component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,23 @@
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
+
 import "./Expenses.css";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangehandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangehandler}
+      />
       {props.items.map((expense) => (
         <ExpenseItem
           key={expense.id}
